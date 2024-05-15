dd72a8c31b3ac136f319527eb35aa9b880073096
DIANA KAREN BASILIO BELTRAN <dbasilio@100-dbasilio.local>
2024-05-14 18:08:04 -0600

    Added basic style to header and footer.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import styles from "@/styles/Header.module.css";
+import styles from "@/styles/Header.module.scss";
 
 const Header: React.FC = () => {
   return <header className={styles.header}></header>;
