0ad777f824cd3f880922f3ae9c6818f9efa0f9b1
DIANA KAREN BASILIO BELTRAN <dbasilio@100-dbasilio.local>
2024-05-17 02:19:25 -0600

    Title optional.

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,14 +2,14 @@ import styles from "@/styles/Section.module.scss";
 
 interface SectionProps {
   id: string;
-  title: string;
+  title?: string;
   children: React.ReactNode;
 }
 
 const Section: React.FC<SectionProps> = ({ id, title, children }) => {
   return (
     <section id={id} className={styles.section}>
-      <h2>{title}</h2>
+      {title && <h2>{title}</h2>}
       {children}
     </section>
   );
